327e1a1a70625cedd224dc478ee99668859d22da
Gromova <malyarchyk.17@gmail.com>
2018-12-24 16:31:55 +0200

    Update default state

--- src/game/core/default_state.js
+++ src/game/core/default_state.js
@@ -3,6 +3,31 @@ import _ from 'lodash';
 
 
 const default_state = {
+    money: 4200,
+
+    constructions: {
+        innerLuggageCart: 1,
+        outLuggageCart: 1,
+        innerEscalator: 1,
+        outEscalator: 1,
+        innerFastRoad: 1,
+        outFastRoad: 1,
+        dutyFree: 1,
+        innerBus: 1,
+        outBus: 1,
+        innerPassport: 1,
+        outPassport: 1,
+        innerSecurity: 1,
+        outSecurity: 1,
+        checkIn: 1,
+        hotel: 1,
+        hall: 1,
+        runway: 1,
+        cafe: 1,
+        rail: 1,
+        parking: 1
+    },
+
     game_speed: 1000,
     frame_rate: 1,
     game_speed_multiplier: 1,
