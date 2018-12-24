729d43c59e4eb0e2e8b1785f3c98fe28f8760922
Gromova <malyarchyk.17@gmail.com>
2018-12-24 16:46:55 +0200

    Update Gin

--- src/bdcgin/Gin.js
+++ src/bdcgin/Gin.js
@@ -1,245 +1,176 @@
-import _ from "lodash";
 
-import Lamp from "./Lamp";
+import _ from 'lodash';
 
 export class Gin {
-    constructor(game_name, stateGenerator = () => {}) {
+    constructor(game_name, stateGenerator = () => {} ) {
         this.game_name = game_name;
 
-        this.store = {
+        this.store = _.assign({
             game_speed: 60000,
             frame_rate: 60,
             game_speed_multiplier: 1,
             frame: 0,
             tick: 0,
             game_paused: true
-        };
+        }, stateGenerator());
 
         this.stateGenerator = stateGenerator;
 
-        this.params = { gin: this };
-
         this.views = [];
-        this.rules = {};
+        this.rules = [];
 
         this.timerID = 0;
         this.publish_timerID = 0;
         this.recently_updated = false;
+
+        //this.playGame = this.playGame.bind(this);
+        //this.pauseGame = this.pauseGame.bind(this);
+        //this.setGameSpeed = this.setGameSpeed.bind(this);
+        //this.frame = this.frame.bind(this);
+        //this.tick = this.tick.bind(this);
+
     }
-
-    init = () => {
-        Lamp.register(this);
-        this.loadDefaultGame();
-        document.addEventListener("keydown", e => {
-            if (e.code === "Space") {
-                if (this.store.game_paused) {
-                    this.playGame();
-                } else {
-                    this.pauseGame();
-                }
-            }
-        });
-        //this.store = _.assign(this.store, this.stateGenerator());
-    };
-
-    addDefaultStateGenerator = stateGenerator => {
+    
+    addDefaultStateGenerator = (stateGenerator) => {
         this.stateGenerator = stateGenerator;
     };
 
-    addViewHandler = stateUpdater => {
-        this.views.push({
-            setState: new_state => {
-                return stateUpdater(new_state);
-            }
-        });
+    addViewHandler = (stateUpdater) => {
+        this.views.push({setState: (new_state) => { return stateUpdater(new_state); }});
     };
 
-    addViewApp = ViewApp => {
+    addViewApp = (ViewApp) => {
         this.views.push(ViewApp);
     };
 
-    connectReact = App => {
+    connectReact = (App) => {
         this.addViewApp(App);
     };
 
-    getCurrentSaveName = () => localStorage.getItem(this.game_name + "_current_save_name");
-    setCurrentSaveName = name => localStorage.setItem(this.game_name + "_current_save_name", name);
-    getCurrentSaveJSON = () => JSON.parse(localStorage.getItem(this.game_name + "_save_" + this.getCurrentSaveName()));
-    setCurrentSaveJSON = state => localStorage.setItem(this.game_name + "_save_" + this.getCurrentSaveName(), JSON.stringify(state));
+    loadGame = (save_name = '') => {
+        localStorage.setItem(this.game_name + "_current_save_name", save_name);
+        var app_state = JSON.parse(localStorage.getItem(this.game_name + "_save_" + save_name));
+        this.setState(app_state ? app_state : this.stateGenerator());
+    };
+
+    saveGame = (state) => {
+        let save_name = localStorage.getItem(this.game_name + "_current_save_name");
+        localStorage.setItem(this.game_name + "_save_" + save_name, JSON.stringify(state));
+    };
 
     newGame = () => {
         // if (!window.confirm('Are you ready to start a new game? Your progress will be lost.')) return false;
-        let save_name = this.getCurrentSaveName();
-        this.clearSave(save_name); // localStorage.setItem(this.game_name + "_save_" + (save_name ? save_name : 0), null);
+        let save_name = localStorage.getItem(this.game_name + "_current_save_name");
+        localStorage.setItem(this.game_name + "_save_" + save_name, null);
         this.setState(this.stateGenerator());
-        this.initLoop();
-    };
-
-    newGameAs = (save_name = 0) => {
-        this.setCurrentSaveName(save_name);
-        this.setState(this.stateGenerator());
-    };
-
-    loadGame = (save_name = 0) => {
-        this.setCurrentSaveName(save_name);
-        var app_state = JSON.parse(localStorage.getItem(this.game_name + "_save_" + save_name));
-        this.setState(_.assign(this.stateGenerator(), _.pickBy(app_state, v => !_.isNaN(v) && v !== undefined)));
-    };
-
-    loadDefaultGame = () => {
-        var app_state = this.getCurrentSaveJSON();
-        this.setState(app_state ? _.assign(this.stateGenerator(), _.pickBy(app_state, v => !_.isNaN(v) && v !== undefined)) : _.assign(this.store, this.stateGenerator()));
-    };
-
-    saveGameAs = (save_name = 0, state) => {
-        localStorage.setItem(this.game_name + "_save_" + save_name, JSON.stringify(state));
-        this.setCurrentSaveName(save_name);
-    };
-
-    saveGame = state => this.setCurrentSaveJSON(state);
-
-    clearSave = save_name => {
-        localStorage.removeItem(this.game_name + "_save_" + save_name);
+        this.playGame();
     };
 
     setState = (next_store, timeout = 0) => {
-        //console.log('setState', next_store, timeout, this.recently_updated);
+        // console.log('setState', next_state, timeout, this.recently_updated);
 
-        this.setStore(next_store); // this.store = _.assign(this.store, next_store);
+        this.store = _.assign(this.store, next_store);
 
         if (timeout === 0 || !this.recently_updated) {
+
             clearInterval(this.publish_timerID);
-            this.publish_timerID = setTimeout(() => {
-                this.recently_updated = false;
-            }, timeout);
+            this.publish_timerID = setTimeout(
+                () => { this.recently_updated = false; }, timeout);
             this.recently_updated = true;
-            _.each(this.views, view => view.setState(_.cloneDeep(this.store)));
+            this.saveGame(this.store);
+            _.each(this.views, view => view.setState(_.cloneDeep(this.store)) );
         }
     };
 
-    setStore = next_store => {
-        //console.log("set store " + this.store.tick);
-        //console.log(next_store);
-        this.store = _.assign(this.store, _.pickBy(next_store, v => !_.isNaN(v) && v !== undefined));
-    };
-
-    exec = (helper, params = {}) => {
-        this.perform(helper, _.assign(this.params, params));
-    };
-
-    perform = (helper, ...args) => {
-        this.setState(helper(this.store, ...args));
-    };
-
-    onClick = item => {
-        //console.log(item);
+    onClick = (item) => {
         let store = this.store;
         if (item.isDisabled && item.isDisabled(store)) {
             return false;
         }
         if (item.cost) {
             if (isEnough(store, item.cost)) {
-                if (item.onClick) this.setState(item.onClick(chargeCost(store, item.cost), this.params), 25);
-            } else {
+                if (item.onClick) this.setState(item.onClick(chargeCost(store, item.cost), {gin: this}), 0);
+            }
+            else {
                 return false;
             }
-        } else {
-            if (item.onClick) this.setState(item.onClick(store, this.params), 25);
+        }
+        else {
+            if (item.onClick) this.setState(item.onClick(store, {gin: this}), 0);
         }
     };
-
-    initLoop = () => {
-        if (!this.store.game_paused) {
-            clearInterval(this.timerID);
-            this.timerID = setInterval(
-                () => this.onInterval(),
-                Math.floor(this.store.game_speed / this.store.frame_rate / this.store.game_speed_multiplier)
-            );
-        }
-    };
-
-    playGame = () => {
-        this.setState({ game_paused: false });
-        this.initLoop();
+    
+    playGame = (speed_multiplier = false) => {
+        clearInterval(this.timerID);
+        this.timerID = setInterval(
+            () => this.onInterval(),
+            Math.floor(this.store.game_speed
+                / this.store.frame_rate
+                / (speed_multiplier ? speed_multiplier : this.store.game_speed_multiplier))
+        );
+        this.setState({game_paused: false});
     };
 
     pauseGame = () => {
         clearInterval(this.timerID);
-        this.setState({ game_paused: true });
+        this.setState({game_paused: true});
     };
 
-    setGameSpeed = speed => {
-        this.setState({ game_speed_multiplier: speed });
-        if (!this.store.game_paused) this.playGame();
+    setGameSpeed = (speed) => {
+        this.setState({game_speed_multiplier: speed});
+        if (!this.store.game_paused) this.playGame(speed);
     };
-
-    registerRule = (key, rule) => {
-        this.rules[key] = rule;
+    
+    registerRule = (rule) => {
+        this.rules.push(rule);
     };
-
-    registerRules = rules => {
-        _.each(rules, (rule, key) => this.registerRule(key, rule));
+    
+    registerRules = (rules) => {
+        _.each(rules, rule => this.registerRule(rule));
     };
-
+    
     onInterval = () => {
         let store = this.store;
-        // console.log('onInterval 1', store);
+        // console.log('onInterval', store, this.rules, this.views);
+        // console.log(store);
 
         if (store.frame % store.frame_rate === 0) {
             store = this.onTick(store);
             store.tick++;
         }
 
-        // console.log('onInterval 2', store);
-
         store = this.onFrame(store);
         store.frame++;
+
         //    localStorage.setItem(game_name+"_app_state", JSON.stringify(store));
         this.setState(store, 250);
-        // console.log("save_game");
-        this.saveGame(this.store);
     };
 
-    onFrame = store => {
+    onFrame = (store) => {
         // console.log('onFrame', store);
-        _.each(this.rules, (item, key) => {
-            if (item.onFrame) {
-                let next_store = item.onFrame(store, this.params);
-                // console.log('tick', key, next_store);
-                if (next_store) {
-                    store = next_store;
-                } else {
-                    console.log("!!!BROKEN RULE!!!", key, next_store);
-                }
-            }
+        _.each(this.rules, (item) => {
+            if (item.onFrame) store = item.onFrame(store, {gin: this});
         });
 
         return store;
     };
-
-    onTick = store => {
-        // console.log('onTick', store, this.rules);
-        _.each(this.rules, (item, key) => {
-            if (item.onTick) {
-                let next_store = item.onTick(store, this.params);
-                // console.log('tick', key, next_store);
-                if (next_store) {
-                    store = next_store;
-                } else {
-                    console.log("!!!BROKEN RULE!!!", key, next_store);
-                }
-            }
+    
+    onTick = (store) => {
+        // console.log('onTick', store);
+        _.each(this.rules, (item) => {
+            if (item.onTick) store = item.onTick(store, {gin: this});
         });
 
         return store;
     };
 }
 
+
 export function isEnough(store, cost) {
     let enough = true;
     _.each(cost, (value, resource_key) => {
         if (_.get(store, resource_key) < value) enough = false;
-        // console.log(_.get(store, resource_key), resource_key);
+       // console.log(_.get(store, resource_key), resource_key);
     });
 
     //console.log(store, cost, enough);
@@ -265,14 +196,15 @@ export function gainCost(store, cost) {
 }
 
 export function drawCost(cost) {
-    let text = "";
+    let text = '';
     _.each(cost, (value, resource) => {
         if (value > 0) {
-            text += resource + ": " + value + " ";
+            text += resource + ': ' + value + ' ';
         }
     });
     return text;
 }
+
 
 export function obtain(store, key) {
     console.log(store, key, store.tech[key]);
--- src/bdcgin/GinGameMenu.js
+++ src/bdcgin/GinGameMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import classNames from "classnames";
+import classNames from 'classnames';
 
 export default class GinGameMenu extends Component {
     render() {
@@ -7,46 +7,27 @@ export default class GinGameMenu extends Component {
         let gin = this.props.gin;
         let speeds = this.props.speeds;
 
-        return (
-            <div className="flex-element">
-                <span
-                    onClick={() => {
-                        if (state.game_paused) {
-                            gin.playGame();
-                        } else {
-                            gin.pauseGame();
-                        }
-                    }}
-                >
-                    <span
-                        className={classNames("glyphicon", state.game_paused ? "glyphicon-play" : "glyphicon-pause")}
-                        style={{ width: 28, height: 28 }}
-                    />
+        return <div className="flex-element">
+                <span onClick={() => {
+                    if (state.game_paused) {
+                        gin.playGame();
+                    } else {
+                        gin.pauseGame();
+                    }
+                }}>
+                    <span className={classNames('glyphicon', (state.game_paused ? 'glyphicon-play' : 'glyphicon-pause'))} style={{width: 28, height: 28}}></span>
                 </span>
-                <span>
-                    {speeds.map((speed, index) => {
-                        return (
-                            <span key={index}>
-                                {state.game_speed_multiplier === speed ? (
-                                    <button className="" style={{ width: 42, height: 28 }}>
-                                        <u>x{speeds[index]}</u>
-                                    </button>
-                                ) : (
-                                    <button
-                                        className=""
-                                        style={{ width: 42, height: 28 }}
-                                        onClick={() => {
-                                            gin.setGameSpeed(speed);
-                                        }}
-                                    >
-                                        x{speeds[index]}
-                                    </button>
-                                )}
-                            </span>
-                        );
-                    })}
-                </span>
-            </div>
-        );
+            <span>
+                {speeds.map((speed, index) => {
+                    return <span key={index}>
+                        {state.game_speed_multiplier === speed
+                            ? <button className="" style={{width: 42, height: 28}}><u>x{speeds[index]}</u></button>
+                            : <button className="" style={{width: 42, height: 28}} onClick={() => {
+                            gin.setGameSpeed(speed); }}>x{speeds[index]}
+                        </button>}
+                    </span>
+                })}
+            </span>
+        </div>
     }
 }
