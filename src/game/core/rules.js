d1ab2e8a063d1dd55282cb5000423c3ca7be734c
Gromova <malyarchyk.17@gmail.com>
2019-01-02 17:28:37 +0200

    Refactor rules

--- src/game/core/rules.js
+++ src/game/core/rules.js
@@ -1,10 +1,27 @@
 
 import _ from 'lodash';
+import { constructions } from '../../gamedata/constructions.js';
 
 export const rules = {
-    matrix_show: { onFrame: (store, params = {}) => { store.matrix_show = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15); return store; }},
+    matrix_show: { onFrame: (store, params = {}) => {
+        store.matrix_show =
+            Math.random().toString(36).substring(2, 15) +
+            Math.random().toString(36).substring(2, 15);
+        return store;
+    }},
 
     pause_on_game_end: {onFrame: (store, params) => {
-        if (store.game_end) params.gin.pauseGame(); return store; }},
+        if (store.game_end) params.gin.pauseGame();
+        return store;
+    }},
 
 };
+
+
+/**
+ generate_passengers: { onTick: (store) => {
+        const runway = constructions.runway;
+        runway.passengers += _.random(100, runway.bandwidth);
+        return store;
+    }}
+*/
