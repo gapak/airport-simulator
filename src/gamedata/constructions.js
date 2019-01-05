a91a971cf162e434d75e00a67ec4a82cd7407e7b
Gromova <malyarchyk.17@gmail.com>
2019-01-05 14:16:28 +0200

    Add predicate to move function

--- src/gamedata/constructions.js
+++ src/gamedata/constructions.js
@@ -7,14 +7,14 @@ const buy = (state, item_key) => {
     return state;
 };
 
-const move = (state, direction = {from: '', to: ''}) => {
+const move = (state, direction = {from: '', to: ''}, predicate = passenger => true) => {
     let processedPassengers = _.remove(
         state.processing[direction.from],
-        passenger => passenger.busy_till < state.tick
+        passenger => passenger.busy_till < state.tick && predicate(passenger)
     );
 
     // If passengers are already processed
-    // they are going to queue to the next construction
+    // they are going to queue to the to construction
     if (processedPassengers.length > 0) {
         state.queue[direction.to] = _.concat(state.queue[direction.to], processedPassengers);
     }
@@ -23,7 +23,7 @@ const move = (state, direction = {from: '', to: ''}) => {
     let totalBandwidth = constructions[direction.from].bandwidth * state.constructions[direction.from];
     // passengers that now in the construction
     let numberOfProcessingPassengers = state.processing[direction.from].length;
-    // passengers that are waiting for getting into the next construction
+    // passengers that are waiting for getting into the to construction
     let numberOfPassengersInQueue = state.queue[direction.from].length;
     
     if (numberOfProcessingPassengers < totalBandwidth) {
@@ -33,7 +33,16 @@ const move = (state, direction = {from: '', to: ''}) => {
             // If there are more vacant places than passengers in the queue
             // Then move all the queue
             let numberOfPassengersToMove = Math.min(vacantPlaces, numberOfPassengersInQueue);
-            let passengersToProcessing = _.drop(state.queue[direction.from], numberOfPassengersToMove);
+
+            let passengersToProcessing = _.take(state.queue[direction.from], numberOfPassengersToMove);
+
+            if (numberOfPassengersToMove !== passengersToProcessing.length) {
+                console.log(
+                    "Wrong count: count !== to_processing.length ",
+                    numberOfPassengersToMove,
+                    passengersToProcessing.length
+                );
+            }
 
             // Every passenger gets a time-marker: till when they will be processing
             // from now + @processing_time of the construction
@@ -44,9 +53,9 @@ const move = (state, direction = {from: '', to: ''}) => {
             // Add passengers to processing
             state.processing[direction.from] = _.concat(state.processing[direction.from], passengersToProcessing);
             // Remove passengers from the queue
-            state.queue[direction.from] = _.dropRight(
+            state.queue[direction.from] = _.drop(
                 state.queue[direction.from],
-                (numberOfPassengersInQueue - numberOfPassengersToMove)
+                numberOfPassengersToMove
             );
         }
     }
